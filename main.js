cd403c24b9fda5d7d5cc2e1babd60ebecdd5b5d0
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-20 17:49:49 +0530

    click-function-added

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ const quesDivs = document.querySelectorAll('.faq')
 
 quesDivs.forEach((ques) => {
   ques.addEventListener('mouseover',(e) => {
-      ques.style.color = 'hsl(14, 88%, 65%)'
-
+      ques.firstElementChild.style.color = 'hsl(14, 88%, 65%)'
   })
 })
 
 quesDivs.forEach((ques) => {
   ques.addEventListener('mouseout',(e) => {
-    ques.style.color = 'hsl(0,0%,0%)'
+    ques.firstElementChild.style.color = 'hsl(0,0%,0%)'
+  })
+})
+
+quesDivs.forEach((ques) => {
+  ques.addEventListener('click',() => {
+    const displayprop = ques.lastElementChild.style.display
+    if(!displayprop || displayprop=='none') {
+        ques.parentElement.style.width = '350px'
+        ques.lastElementChild.style.display = 'block'
+    } else {
+      ques.lastElementChild.style.display = 'none'
+    }
+
   })
 })
